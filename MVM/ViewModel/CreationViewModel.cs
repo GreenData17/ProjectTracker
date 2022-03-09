0183f4ad418e9822624a4a0bc521cb7fe0d016f5
GreenData17 <g.greenmail.g@gmail.com>
2022-03-09 01:13:36 +0100

    INIT

---
+++ MVM/ViewModel/CreationViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_Tracker.Core;
+
+namespace Project_Tracker.MVM.ViewModel
+{
+    class CreationViewModel : ObservableObject
+    {
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public string ProjectColor { get; set; }
+
+        // Commands //
+        public RelayCommand CreateProjectCommand { get; set; }
+        public RelayCommand CancelCommand { get; set; }
+
+        public CreationViewModel()
+        {
+            ProjectColor = "#59ecff";
+
+            CreateProjectCommand = new RelayCommand(o => {
+                
+            });
+
+            CancelCommand = new RelayCommand(o => {
+                MainViewModel.instance.CurrentView = new HomeViewModel();
+            });
+        }
+    }
+}
